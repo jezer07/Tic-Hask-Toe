184d0822c9e4c2de4fcfbd8ab2ff0dd47ec211f2
jezer_crespo <jezercrespo@gmail.com>
2022-12-03 06:01:05 +0000

    updated readme

--- solutions/Main.hs
+++ solutions/Main.hs
@@ -7,4 +7,4 @@ import A4
 import A5
 
 main :: IO ()
-main = putStrLn "Welcome to Part I of EMURGO Academy's Haskell course!"
+main = putStrLn "Welcome to Part I of EMURGO Academy's Haskell course, Jezer!"
