51233681b579f70e0c681ac0127e0ccac63928a6
jezer_crespo <jezercrespo@gmail.com>
2022-12-03 06:53:55 +0000

    Solutions to A1-2

--- solutions/A1.hs
+++ solutions/A1.hs
@@ -27,36 +27,50 @@ _SEP_ = ['_','|','_']
 -- *** Assignment 1-2 *** --
 
 -- Q#06
-data Square
+data Square = X | O | EMPTY deriving (Eq, Show)
 
 
 -- Q#07
-data GameState
+data GameState = X_WON | O_WON | TIE | IN_PROGRESS deriving Show
 
 
 -- Q#08
-
-
-
-
-
+type Player = Square
+type Row = [Square]
+type Line = [Square]
+type Board = [Row]
+type Move = (Int, Int)
 
 -- Q#09
+getFirstPlayer:: Bool -> Player
+getFirstPlayer x = if x then X else O
 
-getFirstPlayer = undefined
-
-
-getFirstPlayer_ = undefined
+getFirstPlayer_ :: Bool -> Player
+getFirstPlayer_ x 
+    | x = X
+    | not x = O
+    | otherwise = undefined
 
 -- Q#10
 
-showGameState gs = undefined
+showGameState gs = case gs of 
+    X_WON -> "X Won!"
+    O_WON -> "Y Won!"
+    TIE -> "It's a tie!"
+    IN_PROGRESS -> "Game is in-progress"
+
 
 -- Q#11
 
-switchPlayer = undefined
+switchPlayer p = case p of 
+    X -> O
+    O -> X
+    EMPTY -> EMPTY 
 
 
 -- Q#12
 
-showSquare = undefined
+showSquare sq = case sq of 
+    X -> "X"
+    O -> "O"
+    EMPTY -> "_" 
