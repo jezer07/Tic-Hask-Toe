9969784382fcba66a3bb7de6e2c495abe65ab9ed
jezer_crespo <jezercrespo@gmail.com>
2023-01-21 17:33:46 +0000

    progress

--- solutions/A5.hs
+++ solutions/A5.hs
@@ -11,30 +11,35 @@ import Control.Monad (when)
 -- *** Assignment 5-1 *** --
 
 -- Q#01
-
-printBoard = undefined
+printBoard:: Board -> IO ()
+printBoard = putStrLn . formatBoard 
 
 -- Q#02
 _LOGO_PATH_ :: FilePath
 _LOGO_PATH_ = "./assets/logo.txt"
 
-
-printLogo = undefined
+printLogo:: IO ()
+printLogo =  readFile _LOGO_PATH_ >>= putStrLn
 
 -- Q#03
 _RANDOM_BOOL_ :: IO Bool
 _RANDOM_BOOL_ = uniformM globalStdGen
 
-
-firstPlayer = undefined
+firstPlayer::  IO Player
+firstPlayer = _RANDOM_BOOL_ >>= (\x-> return $ getFirstPlayer x)
 
 -- Q#04
-
-getMove = undefined
+getMove:: Board -> IO Move
+getMove board = getLine >>= (\input ->  processMove input)
+        where 
+            processMove move = if isValidMove board (stringToMove move) 
+                                then return (stringToMove move) 
+                                else putStrLn "Invalid move! Try again" 
+                                >> getMove board
 
 -- Q#05
 
-play = undefined
+play = when _DISPLAY_LOGO_ 
 
 -- *** Assignment 5-2 *** --
 
